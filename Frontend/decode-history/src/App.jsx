850db5e80ce995a4c40376a3e5d3b14a3c874ead
YAROSLAV VOVK <100441284+Underded29@users.noreply.github.com>
2026-05-20 23:04:35 +0300

    set up front-end

---
+++ Frontend/decode-history/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css'; // Залишаємо глобальні стилі
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>DeCode: History</h1>
+      {/* Тут згодом буде маршрутизація (Header, Main, Footer) */}
+    </div>
+  );
+}
+
+export default App;
